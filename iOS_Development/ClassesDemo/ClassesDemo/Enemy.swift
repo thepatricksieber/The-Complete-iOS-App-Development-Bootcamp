611c10236925a4bacc860f82222255d370f2d43b
Patrick Sieber <31919032+thepatricksieber@users.noreply.github.com>
2020-04-11 10:32:49 +0200

    All changes saved to github

---
+++ iOS_Development/ClassesDemo/ClassesDemo/Enemy.swift
@@ -0,0 +1,9 @@
+//
+//  Enemy.swift
+//  ClassesDemo
+//
+//  Created by Patrick Sieber on 09.04.20.
+//  Copyright © 2020 Patrick Sieber. All rights reserved.
+//
+
+import Foundation
---
+++ iOS_Development/ClassesDemo/ClassesDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ClassesDemo
+//
+//  Created by Patrick Sieber on 09.04.20.
+//  Copyright © 2020 Patrick Sieber. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
